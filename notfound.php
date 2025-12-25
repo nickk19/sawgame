52829488d80a306f84f44ee9de384359f5631cfc
Nicolò Ceretti <nikko8204@gmail.com>
2025-12-25 18:32:28 +0100

    Added 404 page

---
+++ notfound.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,minimum-scale=1">
+		<title>Home</title>
+		<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<header class="header">
+			<div class="wrapper">
+				<h1>SAWGame Home</h1>
+
+				<nav class="menu">
+					<a href="home.php">Home</a>
+					<a href="game.php">Gioca</a>
+					<a href="profile.php">Profilo</a>
+					<a href="logout.php">Logout</a>
+				</nav>
+			</div>
+		</header>
+
+		<div class="content">
+			<div class="page-title">
+				<div class="wrap">
+					<h2>404: Oh, no. La risorsa che stai cercando non esiste (per ora)!</h2>
+				</div>
+			</div>
+
+			<div class="block">
+				<p>Probabilmente sto giocando a <a href="https://www.reachthefinals.com/">The Finals</a> invece di implementarla</p>
+			</div>
+		</div>
+	</body>
+</html>
